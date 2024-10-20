373ef7d72d3f0629fbdf81dd50d3f4dfc3e91738
lucyfinnerty <114194409+lucyfinnerty@users.noreply.github.com>
2024-10-19 22:11:16 -0500

    moved java projs to new java folder

---
+++ java/bank/SavingsAccount.java
@@ -0,0 +1,34 @@
+public class SavingsAccount {
+	
+	String name;
+	double balance;
+
+	public SavingsAccount(String name, double balance){
+		this.name = name;
+		this.balance = balance;
+	}
+	public String getName(){
+		return name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public double getBalance(){
+		return balance;
+	}
+	public int getRoutingNumber() {
+        return 0;
+    }
+    public int getAccountNumber() {
+        return 0;
+    }
+	public void deposit(double money) {
+		this.balance += money;
+	}
+	public void withdraw(double money){
+		this.balance -= money;
+	}
+	public String printAccountSummary(){
+		return null;
+	}
+}
